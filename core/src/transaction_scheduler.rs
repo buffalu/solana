031de8095fa452cf9bbcabaf13ff36b30004b3e6
Lucas B <lucas@jito.wtf>
2022-05-15 18:57:22 -0500

    mas

--- core/src/transaction_scheduler.rs
+++ core/src/transaction_scheduler.rs
@@ -13,22 +13,26 @@ use {
     },
 };
 
-pub struct RequestBatchMessage {
-    num_txs: usize,
-    response_sender: Sender<RequestBatchResponse>,
+pub enum SchedulerMessage {
+    RequestBatch { num_txs: usize },
 }
 
-pub struct RequestBatchResponse {}
+pub struct SchedulerRequest {
+    msg: SchedulerMessage,
+    response_sender: Sender<SchedulerResponse>,
+}
+
+pub struct SchedulerResponse {}
 
 pub struct TransactionScheduler {
     tx_request_handler_thread: JoinHandle<()>,
-    tx_batch_request_sender: Sender<RequestBatchMessage>,
+    tx_scheduler_request_sender: Sender<SchedulerRequest>,
 
     tpu_vote_request_handler_thread: JoinHandle<()>,
-    tpu_vote_tx_batch_request_sender: Sender<RequestBatchMessage>,
+    tpu_vote_scheduler_request_sender: Sender<SchedulerRequest>,
 
     gossip_vote_request_handler_thread: JoinHandle<()>,
-    gossip_vote_tx_batch_request_sender: Sender<RequestBatchMessage>,
+    gossip_vote_scheduler_request_sender: Sender<SchedulerRequest>,
 }
 
 impl TransactionScheduler {
@@ -37,41 +41,41 @@ impl TransactionScheduler {
         verified_tpu_vote_packets_receiver: Receiver<Vec<PacketBatch>>,
         verified_gossip_vote_packets_receiver: Receiver<Vec<PacketBatch>>,
     ) -> Self {
-        let (tx_batch_request_sender, tx_batch_request_receiver) = unbounded();
+        let (tx_scheduler_request_sender, tx_scheduler_request_receiver) = unbounded();
         let tx_request_handler_thread = Self::start_event_loop(
             "tx_scheduler_insertion_thread",
-            tx_batch_request_receiver,
+            tx_scheduler_request_receiver,
             verified_receiver,
         );
 
-        let (tpu_vote_tx_batch_request_sender, tpu_vote_tx_batch_request_receiver) = unbounded();
+        let (tpu_vote_scheduler_request_sender, tpu_vote_scheduler_request_receiver) = unbounded();
         let tpu_vote_request_handler_thread = Self::start_event_loop(
             "tpu_vote_scheduler_tx_insertion_thread",
-            tpu_vote_tx_batch_request_receiver,
+            tpu_vote_scheduler_request_receiver,
             verified_tpu_vote_packets_receiver,
         );
 
-        let (gossip_vote_tx_batch_request_sender, gossip_vote_tx_batch_request_receiver) =
+        let (gossip_vote_scheduler_request_sender, gossip_vote_scheduler_request_receiver) =
             unbounded();
         let gossip_vote_request_handler_thread = Self::start_event_loop(
             "gossip_vote_scheduler_tx_insertion_thread",
-            gossip_vote_tx_batch_request_receiver,
+            gossip_vote_scheduler_request_receiver,
             verified_gossip_vote_packets_receiver,
         );
 
         TransactionScheduler {
             tx_request_handler_thread,
-            tx_batch_request_sender,
+            tx_scheduler_request_sender,
             tpu_vote_request_handler_thread,
-            tpu_vote_tx_batch_request_sender,
+            tpu_vote_scheduler_request_sender,
             gossip_vote_request_handler_thread,
-            gossip_vote_tx_batch_request_sender,
+            gossip_vote_scheduler_request_sender,
         }
     }
 
     fn start_event_loop(
         t_name: &str,
-        request_batch_receiver: Receiver<RequestBatchMessage>,
+        scheduler_request_receiver: Receiver<SchedulerRequest>,
         packet_receiver: Receiver<Vec<PacketBatch>>,
     ) -> JoinHandle<()> {
         Builder::new()
@@ -84,17 +88,17 @@ impl TransactionScheduler {
                         recv(packet_receiver) -> maybe_packet_batches => {
                             match maybe_packet_batches {
                                 Ok(packet_batches) => {
-                                    Self::buffer_packet_batches(&mut unprocessed_packet_batches, packet_batches);
+                                    Self::handle_packet_batches(&mut unprocessed_packet_batches, packet_batches);
                                 }
                                 Err(_) => {
                                     break;
                                 }
                             }
                         }
-                        recv(request_batch_receiver) -> maybe_batch_request => {
+                        recv(scheduler_request_receiver) -> maybe_batch_request => {
                             match maybe_batch_request {
                                 Ok(batch_request) => {
-                                    Self::handle_batch_request(&mut unprocessed_packet_batches, batch_request)
+                                    Self::handle_scheduler_request(&mut unprocessed_packet_batches, batch_request)
                                 }
                                 Err(_) => {
                                     break;
@@ -107,13 +111,20 @@ impl TransactionScheduler {
             .unwrap()
     }
 
-    fn handle_batch_request(
-        unprocessed_packets: &mut UnprocessedPacketBatches,
-        batch_request: RequestBatchMessage,
+    /// Handles scheduler requests and sends back a response over the channel
+    fn handle_scheduler_request(
+        _unprocessed_packets: &mut UnprocessedPacketBatches,
+        scheduler_request: SchedulerRequest,
     ) {
+        let _response_sender = scheduler_request.response_sender;
+
+        match scheduler_request.msg {
+            SchedulerMessage::RequestBatch { .. } => {}
+        }
     }
 
-    fn buffer_packet_batches(
+    /// TODO(LB): probably want to have the same forwarding, hold and forward, etc. logic here
+    fn handle_packet_batches(
         unprocessed_packets: &mut UnprocessedPacketBatches,
         packet_batches: Vec<PacketBatch>,
     ) {
@@ -135,25 +146,28 @@ impl TransactionScheduler {
         info!("dropped {} transactions", number_of_dropped_packets);
     }
 
-    pub fn request_tx_vote_batch(self, num_txs: usize) -> RequestBatchResponse {
-        Self::request_batch_internal(&self.tx_batch_request_sender, num_txs)
+    /// Requests a batch containing up to num_txs transactions
+    pub fn request_tx_batch(self, num_txs: usize) -> SchedulerResponse {
+        Self::request_batch_internal(&self.tx_scheduler_request_sender, num_txs)
     }
 
-    pub fn request_tpu_vote_batch(self, num_txs: usize) -> RequestBatchResponse {
-        Self::request_batch_internal(&self.tpu_vote_tx_batch_request_sender, num_txs)
+    /// Requests a batch containing up to num_txs vote transactions from the tpu_vote port
+    pub fn request_tpu_vote_batch(self, num_txs: usize) -> SchedulerResponse {
+        Self::request_batch_internal(&self.tpu_vote_scheduler_request_sender, num_txs)
     }
 
-    pub fn request_gossip_vote_batch(self, num_txs: usize) -> RequestBatchResponse {
-        Self::request_batch_internal(&self.gossip_vote_tx_batch_request_sender, num_txs)
+    /// Requests a batch containing up to num_txs vote transactions from gossip
+    pub fn request_gossip_vote_batch(self, num_txs: usize) -> SchedulerResponse {
+        Self::request_batch_internal(&self.gossip_vote_scheduler_request_sender, num_txs)
     }
 
     fn request_batch_internal(
-        request_sender: &Sender<RequestBatchMessage>,
+        request_sender: &Sender<SchedulerRequest>,
         num_txs: usize,
-    ) -> RequestBatchResponse {
+    ) -> SchedulerResponse {
         let (response_sender, response_receiver) = unbounded();
-        let request = RequestBatchMessage {
-            num_txs,
+        let request = SchedulerRequest {
+            msg: SchedulerMessage::RequestBatch { num_txs },
             response_sender,
         };
         let _ = request_sender.send(request).unwrap();
@@ -163,6 +177,7 @@ impl TransactionScheduler {
     pub fn join(self) -> thread::Result<()> {
         self.tx_request_handler_thread.join()?;
         self.tpu_vote_request_handler_thread.join()?;
+        self.gossip_vote_request_handler_thread.join()?;
         Ok(())
     }
 }
